54960e394661447611b4e89f1156b8ed5baf7da5
Stephanie Mathis <stephaniedmathis@gmail.com>
2022-04-19 20:41:07 -0400

    update routes and models code

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -36,12 +36,11 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       // allowNull: false,
       references: {
         model: 'category',
         key: 'id',
-        unique: false
       }
     }
   },
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
@@ -14,10 +14,20 @@ ProductTag.init(
       autoIncrement: true
     },
     product_id: {
-      type:DataTypes.STRING,
-      allowNull:false
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id"
+      }
     }
-  },    
+  },
   {
     sequelize,
     timestamps: false,
